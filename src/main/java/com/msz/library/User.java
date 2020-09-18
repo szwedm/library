8f405c80eff16f34d8977a9ef85eb9bffe59fc90
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2020-09-18 15:57:38 +0200

    User model with JPA annotations

---
+++ src/main/java/com/msz/library/User.java
@@ -0,0 +1,67 @@
+package com.msz.library;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    private char[] pass;
+
+    protected User() {
+    }
+
+    public User(String name, String email, String pass) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass.toCharArray();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char[] getPass() {
+        return pass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
