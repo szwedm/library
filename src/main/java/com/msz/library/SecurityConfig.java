5ecac7ec93bd0d229c429dedd104cd3b72d4a47e
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2020-10-21 17:07:29 +0200

    SecurityConfig: in-memory admin authentication

--- src/main/java/com/msz/library/SecurityConfig.java
+++ src/main/java/com/msz/library/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("super123")).roles("ADMIN");
     }
 
     @Bean
